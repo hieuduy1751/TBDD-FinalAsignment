21d5c689b3bf3023507447db2e2af3e471341167
Duy Hieu <hieuduy1751@gmail.com>
2022-05-17 11:16:46 +0700

    FInished

---
+++ app/src/main/java/com/example/finalassignment/sqlite/TaskContact.java
@@ -0,0 +1,9 @@
+package com.example.finalassignment.sqlite;
+
+import android.provider.BaseColumns;
+
+public class TaskContact implements BaseColumns {
+    public static final String TABLE_NAME = "tasks";
+    public static final String COLUMN_NAME_TITLE = "title";
+    public static final String COLUMN_NAME_SUBTITLE = "subtitle";
+}
